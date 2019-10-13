6ed1754ca711de5f485cb1a45aada13123bc8b2a
Ather Tahir <ather.tahir@bentley.com>
2019-10-13 15:24:05 +0500

    added

--- prepareContainer.sh
+++ prepareContainer.sh
@@ -1,13 +1,13 @@
-pip install --upgrade pip
-pip install matplotlib_venn 
+# pip install --upgrade pip
+# pip install matplotlib_venn 
 
 
-pip install keras
-pip install Pillow
-pip install opencv-python
-pip install mtcnn
-pip install git+https://github.com/rcmalli/keras-vggface.git
+# pip install keras
+# pip install Pillow
+# pip install opencv-python
+# pip install mtcnn
+# pip install git+https://github.com/rcmalli/keras-vggface.git
 
 
-conda install theano
-conda install -c conda-forge tensorflow
+# conda install theano
+# conda install -c conda-forge tensorflow
