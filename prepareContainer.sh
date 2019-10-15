032f0b5daedec1f0a73e1c9eb6521e28e226b74c
Ather Tahir <ather.tahir@bentley.com>
2019-10-15 18:52:16 +0500

    added

--- prepareContainer.sh
+++ prepareContainer.sh
@@ -7,13 +7,16 @@
 
 pip install opencv-python
 pip install mtcnn
+pip install numpy==1.16.0
 
+pip install pydot
+pip install graphviz
+ 
 # pip install git+https://github.com/rcmalli/keras-vggface.git
 
 
 # conda install theano
 # conda install -c conda-forge tensorflow
-
 
 # conda install graphviz
 conda install pydotplus -y
