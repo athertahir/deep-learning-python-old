ddf638787b2cba69a09ce7a990a8b9ed1bc5b3fe
Ather Tahir <ather.tahir@bentley.com>
2019-10-15 19:10:23 +0500

    added

--- prepareContainer.sh
+++ prepareContainer.sh
@@ -5,18 +5,23 @@
 # pip install keras
 # pip install Pillow
 
+echo 'Installing Dependencies'
+
 pip install opencv-python
 pip install mtcnn
 pip install numpy==1.16.0
 
 pip install pydot
 pip install graphviz
- 
+
 # pip install git+https://github.com/rcmalli/keras-vggface.git
 
+sudo add-apt-repository ppa:gviz-adm/graphviz-dev
+sudo apt-get update
+sudo apt-get install graphviz-dev
 
 # conda install theano
 # conda install -c conda-forge tensorflow
-
 # conda install graphviz
-conda install pydotplus -y
+# conda install pydotplus -y
+# conda install graphviz -y
